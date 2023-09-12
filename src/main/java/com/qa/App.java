ab6e1a73bab95ecab8b28a17e350e1403fd54df9
Chandramouli Mahadevan <mouli971@gmail.com>
2023-09-12 14:26:44 +0100

    Added new hello callouts

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,10 +9,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
+        // comment line
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Pranav"));
+        System.out.println(sayHelloToSomeone("LBG"));
         System.out.println(sayGoodbye());
     }
 
